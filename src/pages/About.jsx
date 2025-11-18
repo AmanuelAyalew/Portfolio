0021e6c70e32a7e2667bee2c74208641bf2b0611
AmanuelAyalew <amanuelayalewby7@gmail.com>
2025-11-18 22:10:01 +0300

    Update portfolio project structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+const About = () => {
+  return (
+    <div>
+     <p>About Page</p>
+    </div>
+  );
+};
+
+export default About;
